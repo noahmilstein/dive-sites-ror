899139352ef989be999f358bd51c102598327778
noahmilstein <casanoah@gmail.com>
2016-12-19 18:33:51 -0500

    geocoding conundrum continues

---
+++ app/assets/javascripts/location-geocode.js
@@ -0,0 +1,6 @@
+// user fills out location field as string
+// event listener on click triggers ajax get request to gmaps api
+// returns lat and long
+// callback function is a post request to the dives new (with lat and long of locus)
+// update @divesites to include only those within radius of locus (via geocoder gem)
+// dynamically update dropdown menu with js/jquery
