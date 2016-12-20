bbf5fb6da1a24a49066973f26f28c7842e749eaa
Alex Barstow <alexander.barstow@gmail.com>
2016-12-20 18:04:23 -0500

    React rendering App component

---
+++ react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>IT WORKS!!!</h1>
+    );
+  }
+};
+
+export default App;
