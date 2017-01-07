e59255b9eb84cbfc47a23269cb06c52bd701c4b4
Alex Barstow <alexander.barstow@gmail.com>
2017-01-07 01:09:24 -0500

    fetch/promise refactor wip

--- react/src/App.js
+++ react/src/App.js
@@ -5,89 +5,89 @@ class App extends React.Component {
   super(props);
   this.state = {
     divesites: [],
-    value: '',
-    radius: 0,
     lat: 0.0,
     lng: 0.0
   };
 
-  this.handleValueChange = this.handleValueChange.bind(this);
-  this.handleRadiusChange = this.handleRadiusChange.bind(this);
+  // this.handleValueChange = this.handleValueChange.bind(this);
+  // this.handleRadiusChange = this.handleRadiusChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
 }
 
   componentDidMount() {
-    this.setInitialState()
+    this.fetchDivesites()
   }
 
-  // populates divesites dropdown menu
-  setInitialState() {
-    $.ajax({
-      method: 'GET',
-      url: '/api/v1/divesites.json'
+  // // populates divesites dropdown menu
+  fetchDivesites() {
+    fetch('/api/v1/divesites.json')
+      .then(response => response.json())
+      .then(data => this.setState({divesites: data}))
+  }
+
+  // handleValueChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+  //
+  // handleRadiusChange(event) {
+  //   this.setState({radius: event.target.value});
+  // }
+
+  createSiteList() {
+    const sites = this.state.divesites.filter(site => {
+      
+      return site.name.include()
     })
-    .done(data => {
-      this.setState({divesites: data})
-    });
+    // sites.forEach((site) => {
+    //   sites.push(<li>{site.name}</li>)
+    // })
+    console.log(sites);
+    return sites;
   }
 
-  handleValueChange(event) {
-    this.setState({value: event.target.value});
-  }
+  handleSubmit() {
+    const location = document.querySelector('.location').value;
+    const radius = document.querySelector('.radius').value;
 
-  handleRadiusChange(event) {
-    this.setState({radius: event.target.value});
-  }
-
-  fillDropdown() {
-    let options = [];
-    this.state.divesites.forEach((site) => {
-      options.push(<option>{site.name}</option>)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyCOq298WuE1r_3LH7fwCRW2gJGhohj2qPQ`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          lat: data.results[0].geometry.location.lat,
+          lng: data.results[0].geometry.location.lng
+        })
+      })
+      .then(createSiteList)
     })
-    console.log(options);
-    return options;
-  }
-
-  handleSubmit(event) {
-    const location = this.state.value;
-    event.preventDefault();
-
-    $.ajax({
-      method: 'GET',
-      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${GEOCODE_API_KEY}`
-    })
-    .done(data => {
-      this.setState({
-        lat: data.results[0].geometry.location.lat,
-        lng: data.results[0].geometry.location.lng
-      });
-      const updatedSites = [];
-      // google gives us centerpoint, we have radius
-      // create LatLng objects of divesites
-      // use computeDistanceBetween to create new array of divesites
-      // with only those within the specified radius
-      // set new divesites state
-    });
-    this.fillDropDown()
-    alert(`A name was submitted: ${location}`);
+    //   const updatedSites = [];
+    //   // google gives us centerpoint, we have radius
+    //   // create LatLng objects of divesites
+    //   // use computeDistanceBetween to create new array of divesites
+    //   // with only those within the specified radius
+    //   // set new divesites state
+    // });
+    // this.fillDropDown()
   }
 
   render() {
-    const options = this.fillDropdown();
+    const sites = '';
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Location:
-          <input type="text" value={this.state.value} onChange={this.handleValueChange} />
-        </label>
-        <label>
-          <input type="number" value={this.state.radius} onChange={this.handleRadiusChange} min="1" />
-        </label>
-        <select>
-          {options}
-        </select>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Location:
+            <input className="location" type="text" />
+          </label>
+          <label>
+            Radius:
+            <input className="radius" type="number" min="1" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ul>
+          {sites}
+        </ul>
+      </div>
     );
   }
 };
